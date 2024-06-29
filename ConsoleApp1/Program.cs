a5e0ff6676ee2fd4111c3fcdde3210eaeddc888f
andysheu <andysheu77@hotmail.com>
2024-06-29 10:19:37 +0800

    new project

---
+++ ConsoleApp1/Program.cs
@@ -0,0 +1,21 @@
+﻿using Azure;
+using Azure.AI.OpenAI;
+using OpenAI.Chat;
+
+string endpoint = "https://myaoaiclassdemo.openai.azure.com";
+string key = "daf7070cfbc04099a6317d393f3a9d2d";
+
+var azureOpenAIclient = new AzureOpenAIClient(new Uri(endpoint), new AzureKeyCredential(key));
+
+var chatClient = azureOpenAIclient.GetChatClient("MyGPT4o");
+
+
+ChatCompletion completion = chatClient.CompleteChat(
+    new List<ChatMessage>()
+    {
+        new UserChatMessage("Hi, 你能夠幫助我嗎?"),
+    });
+
+
+Console.WriteLine($"{completion.Role}: {completion.Content[0].Text}");
+
