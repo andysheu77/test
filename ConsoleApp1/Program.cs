24b31672968ef24b662725471dfc940d689f68cc
andysheu <andysheu77@hotmail.com>
2024-07-06 10:08:53 +0800

    使用範例
    新增範例以您想要的回應顯示聊天。它會嘗試模擬您在這裡新增的任何回應，因此請確定它們符合您於系統訊息中設定的規則。

--- ConsoleApp1/Program.cs
+++ ConsoleApp1/Program.cs
@@ -13,8 +13,21 @@ var chatClient = azureOpenAIclient.GetChatClient("MyGPT4o");
 ChatCompletion completion = chatClient.CompleteChat(
     new List<ChatMessage>()
     {
-        new SystemChatMessage("如果沒有說 請 就拒絕回答問題"),
-        new UserChatMessage("Hi, 你能夠幫助我嗎?"),
+        new SystemChatMessage("你是一位電影評論員，請根據使用者的評論好壞，並針對輸入的 <評論> 判斷與進行反駁，反駁的文字應在 20 字內"),
+
+        new UserChatMessage("這部真可怕"),
+        new AssistantChatMessage("好，這是鬼片當然可怕"),
+
+        new UserChatMessage("這部真有趣"),
+        new AssistantChatMessage("好，真是反串這部鬼片"),
+
+        new UserChatMessage("這部真美妙"),
+        new AssistantChatMessage("壞，鬼片怎會很美妙"),
+
+        new UserChatMessage("這部看的很討厭"),
+        new AssistantChatMessage("壞，鬼片是應該被討厭"),
+
+        new UserChatMessage("這部看的很討喜"),
     });
 
 
